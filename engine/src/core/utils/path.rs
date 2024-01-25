009224b4594b2b8b0e8bf7f010097a5c7ab2887c
Simon Rodrigues <simond.rodrigues@gmail.com>
2024-01-25 21:59:46 +0100

    timer struct that can manage ticks and delta (#4)

--- engine/src/core/utils/path.rs
+++ engine/src/core/utils/path.rs
@@ -1,10 +1,8 @@
-use std::path::PathBuf;
-
-pub fn to_abs_path(rel_path: &str) -> Result<PathBuf, std::io::Error> {
+pub fn to_abs_path(rel_path: &str) -> Result<String, std::io::Error> {
     let mut app_path = ::std::env::current_exe()?;
 
     app_path.pop();
     app_path.push(rel_path);
 
-    Ok(app_path)
+    Ok(app_path.to_string_lossy().to_string())
 }
--- engine/src/core/utils/time.rs
+++ engine/src/core/utils/time.rs
@@ -1,7 +1,38 @@
 use std::time::{
+    Instant,
     SystemTime,
     SystemTimeError
 };
+
+#[derive(Debug)]
+pub struct Timer {
+    timer: Instant,
+    last_tick: u128,
+}
+
+impl Timer {
+    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        let timer = Instant::now();
+
+        Ok(Self {
+            timer,
+            last_tick: timer.elapsed().as_millis()
+        })
+    }
+
+    pub fn ticks(&self) -> u128 {
+        self.timer.elapsed().as_millis()
+    }
+
+    pub fn delta(&mut self) -> f32 {
+        let ticks = self.ticks();
+        let delta = ticks - self.last_tick;
+
+        self.last_tick = ticks;
+
+        delta as f32 / 1000.0
+    }
+}
 
 pub fn now_milis() -> Result<u128, SystemTimeError> {
     Ok(SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_millis())
